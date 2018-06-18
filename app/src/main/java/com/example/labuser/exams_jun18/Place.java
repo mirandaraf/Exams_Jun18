53af2bbe4d90c2beb1ebf79af86536f24fa2d130
labuser <rafailogloum@gmail.com>
2018-06-18 18:34:59 +0300

    .

---
+++ app/src/main/java/com/example/labuser/exams_jun18/Place.java
@@ -0,0 +1,49 @@
+package com.example.labuser.exams_jun18;
+
+public class Place {
+    private String formatted_address;
+    private String id;
+    private String name;
+    private String opening_hours;
+    private float rating;
+
+    public String getFormatted_address() {
+        return formatted_address;
+    }
+
+    public void setFormatted_address(String formatted_address) {
+        this.formatted_address = formatted_address;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOpening_hours() {
+        return opening_hours;
+    }
+
+    public void setOpening_hours(String opening_hours) {
+        this.opening_hours = opening_hours;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+}
